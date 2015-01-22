a44d94fe7662a1385349b60af9e8a574821c6dff
Sam Marshall <sjmarshy@gmail.com>
2015-01-22 20:32:20 +0000

    add initial files

---
+++ index.js
@@ -0,0 +1,32 @@
+var settings = require('./config/settings');
+var now      = require('./src/now');
+var knex     = require('knex')({
+    dialect: 'sqlite3',
+    connection: {
+        filename: './now.db'
+    }
+});
+
+knex.schema.hasTable('ticks').then(function (exists) {
+    if (!exists) {
+        return knex.schema.createTable('ticks', function (t) {
+            t.increments('id');
+            t.timestamp('tick');
+            t.text('tags');
+        });
+    }
+}).done();
+
+now.on('error', function (err) {
+    console.warn(err);
+});
+
+if (settings.hasOwnProperty('debug') && settings.debug) {
+    now.on('tick', function (time) {
+        now._debugLog();
+    });
+}
+
+function _isDebug(settings) {
+    return settings.hasOwnProperty('debug') && settings.debug;
+}
